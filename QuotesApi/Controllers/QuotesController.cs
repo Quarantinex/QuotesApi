c4d8c12d38011f9e862665c86d89632164ed59ff
Nayak, Abhishek <abhishek_nayak1@dell.com>
2020-02-18 16:15:36 +0530

    Add project files.

---
+++ QuotesApi/Controllers/QuotesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using QuotesApi.Data;
+using QuotesApi.Models;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace QuotesApi.Controllers
+{
+    public class QuotesController : ApiController
+    {
+        QuotesDbContext quotesDbContext = new QuotesDbContext();
+        // GET: api/Quotes
+        public /*IEnumerable<Quote>*/IHttpActionResult Get()
+        {
+            var quotes = quotesDbContext.Quotes;
+            //return quotesDbContext.Quotes;
+            return Ok(quotes);
+        }
+
+        // GET: api/Quotes/5
+        public /*Quote*/IHttpActionResult Get(int id)
+        {
+            var quote = quotesDbContext.Quotes.Find(id);
+            if(quote == null)
+            {
+                return NotFound();
+            }
+            return Ok(quote);
+        }
+
+        // POST: api/Quotes
+        public /*void*/IHttpActionResult Post([FromBody]Quote quote)
+        {
+            quotesDbContext.Quotes.Add(quote);
+            quotesDbContext.SaveChanges();
+            return StatusCode(HttpStatusCode.Created);
+        }
+
+        // PUT: api/Quotes/5
+        public /*void*/IHttpActionResult Put(int id, [FromBody]Quote quote)
+        {
+            var  entity = quotesDbContext.Quotes.FirstOrDefault(q => q.Id == id);
+            if(entity == null)
+            {
+                return BadRequest("No record found against this id");
+            }
+            entity.Title = quote.Title;
+            entity.Author = quote.Author;
+            entity.Description = quote.Description;
+            quotesDbContext.SaveChanges();
+            return Ok("Record Updated Successfully....");
+        }
+
+        // DELETE: api/Quotes/5
+        public /*void*/IHttpActionResult Delete(int id)
+        {
+            var quote = quotesDbContext.Quotes.Find(id);
+            if (quote == null)
+            {
+                return BadRequest("No record found against this id");
+            }
+            quotesDbContext.Quotes.Remove(quote);
+            quotesDbContext.SaveChanges();
+            return Ok("Quote deleted");
+        }
+    }
+}
---
+++ QuotesApi/Models/Quote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuotesApi.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public String Description { get; set; }
+    }
+}
